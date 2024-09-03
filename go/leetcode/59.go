70ab6b7a247a1a107307f41ba4d40d991220cdea
1kasa <3049907027@qq.com>
2024-09-03 22:41:14 +0800

    update: default by script

---
+++ go/leetcode/59.go
@@ -0,0 +1,43 @@
+package leetcode
+
+func generateMatrix(n int) [][]int {
+	res := make([][]int, n)
+	for i := range res {
+		res[i] = make([]int, n)
+	}
+	startX, startY := 0, 0
+	mid := n / 2
+	count, offset := 1, 1
+	for loop := n / 2; loop > 0; loop-- {
+		i, j := startX, startY
+
+		for j = startY; j < n-offset; j++ {
+			res[startX][j] = count
+			count++
+		}
+
+		for i = startX; i < n-offset; i++ {
+			res[i][j] = count
+			count++
+		}
+
+		for ; j > startY; j-- {
+			res[i][j] = count
+			count++
+		}
+
+		for ; i > startX; i-- {
+			res[i][j] = count
+			count++
+		}
+
+		startX++
+		startY++
+		offset++
+	}
+
+	if n%2 == 1 {
+		res[mid][mid] = count
+	}
+	return res
+}
